8f564fd9e69a7407d1905d41496b0aa9635290dc
Amani Mpangirwa JdD <jeandedieuam@gmail.com>
2021-04-30 17:07:14 +0200

    add new design (#87)

--- src/components/common/AuthWrapper/AuthWrapper.jsx
+++ src/components/common/AuthWrapper/AuthWrapper.jsx
@@ -10,11 +10,9 @@ import { Link } from 'react-router-dom';
 import { Grid, Image } from 'semantic-ui-react';
 
 import LogoColored from 'assets/images/2u-money-logo.svg';
-import AdPlaceholderDefault from 'assets/images/AD_V1.jpg';
+import AdPlaceholderDefault from 'assets/images/event-place-holder.jpg';
 import getUserDailyEvent from 'redux/actions/authWrapper';
 import SelectLanguage from 'components/common/SelectLanguage';
-import { HOME_WEBSITE, HOW_IT_WORKS } from 'constants/general';
-import openInNewTab from 'helpers/openInNewTab';
 
 import './auth-landing-page.scss';
 import './spiner.scss';
@@ -48,10 +46,6 @@ const AuthWrapper = ({
         ) : (
           <Image src={AdPlaceholderDefault} />
         )}
-        <div className="overlay-text">
-          <h1>{global.translate('Welcome to 2U money')}</h1>
-          <p>{rightHeadlineText}</p>
-        </div>
       </div>
       <div className="form-wrapper">
         <div className="os-header">
@@ -59,42 +53,12 @@ const AuthWrapper = ({
             <Grid columns="two">
               <Grid.Row>
                 <Grid.Column
-                  mobile={6}
-                  tablet={6}
-                  computer={4}
-                  style={{ display: 'flex', alignItems: 'center' }}
-                  className="logo-container"
-                >
-                  <Image
-                    className="logo"
-                    src={LogoColored}
-                    onClick={() => {
-                      openInNewTab('2u.money');
-                    }}
-                  />
-                </Grid.Column>
-                <Grid.Column
                   mobile={10}
                   tablet={10}
                   computer={12}
                   className="navbar"
                 >
                   <ul className="nav-menu">
-                    <li className="hide-on-small">
-                      <a href={HOME_WEBSITE}>
-                        {global.translate('Home', 134)}
-                      </a>
-                    </li>
-                    <li className="hide-on-small">
-                      <Link to="/marketplace">
-                        {global.translate('Marketplace', 2190)}{' '}
-                      </Link>
-                    </li>
-                    <li className="hide-on-small">
-                      <a href={HOW_IT_WORKS}>
-                        {global.translate('How it works', 2191)}
-                      </a>
-                    </li>
                     <li>
                       {getSupportedLanguagesLoading ? null : (
                         <SelectLanguage
