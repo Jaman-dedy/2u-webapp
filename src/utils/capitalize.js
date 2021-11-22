c5448edecc594aceaf9a835cb42d23f46b85dc82
chrissiemhrk <chrissiemhrk@gmail.com>
2021-11-22 11:34:30 +0200

    fix capitalization issue on redeem voucher verify button

--- src/utils/capitalize.js
+++ src/utils/capitalize.js
@@ -1,1 +1,6 @@
-export default str => `${str[0].toUpperCase()}${str.substr(1)}`;
+export default str => {
+  if (typeof str === 'string' && str.length) {
+    return `${str[0].toUpperCase()}${str.substr(1)}`;
+  }
+  return str;
+};
