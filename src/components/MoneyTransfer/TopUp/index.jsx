cf464d09105a24b98f2cba85eb7bd07c8fda9ea0
chrissiemhrk <chrissiemhrk@gmail.com>
2021-12-01 12:11:01 +0200

    remove irrelevant phone dropdown after selecting a bank

--- src/components/MoneyTransfer/TopUp/index.jsx
+++ src/components/MoneyTransfer/TopUp/index.jsx
@@ -1,16 +1,9 @@
-import InputLoader from 'assets/images/LoaderRectangle.svg';
-import ReusableDrowdown from 'components/common/Dropdown/ReusableDropdown';
-import LoaderComponent from 'components/common/Loader';
-import Message from 'components/common/Message';
-import Wrapper from 'hoc/Wrapper';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
 import NumberFormat from 'react-number-format';
 import PhoneInput from 'react-phone-input-2';
 import { useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import { updateMoneyTransferStep } from 'redux/actions/dashboard/dashboard';
-import { clearConfirmation } from 'redux/actions/moneyTransfer/confirmTransaction';
 import {
   Button,
   Checkbox,
@@ -19,9 +12,17 @@ import {
   Label,
   Modal,
 } from 'semantic-ui-react';
+import InputLoader from 'assets/images/LoaderRectangle.svg';
+import ReusableDrowdown from 'components/common/Dropdown/ReusableDropdown';
+import LoaderComponent from 'components/common/Loader';
+import Message from 'components/common/Message';
+import Wrapper from 'hoc/Wrapper';
+import { updateMoneyTransferStep } from 'redux/actions/dashboard/dashboard';
+import { clearConfirmation } from 'redux/actions/moneyTransfer/confirmTransaction';
 import countryCodes from 'utils/countryCodes';
 import formatNumber from 'utils/formatNumber';
 import { getPossibleDates } from 'utils/monthdates';
+import { BANK_TRANSFER, PAYPAL, WARI } from 'constants/general';
 import ConfirmationForm from '../../ConfirmationForm';
 import '../SendMoney/modal.scss';
 import TransactionEntity from '../SendMoney/TransactionEntity';
@@ -254,8 +255,8 @@ const TopUpModal = ({
     if (step === 1) {
       if (
         currentProviderOption?.Category === '0' ||
-        currentProviderOption?.Category === '19' ||
-        currentProviderOption?.Category === '7'
+        currentProviderOption?.Category === WARI ||
+        currentProviderOption?.Category === PAYPAL
       ) {
         setButtonAction(global.translate('Transfer money'));
         setVerifyAccount(false);
@@ -402,7 +403,7 @@ const TopUpModal = ({
             </div>
 
             <div className="phone-bank">
-              {currentProviderOption?.Category === '4' ? (
+              {currentProviderOption?.Category === BANK_TRANSFER ? (
                 <>
                   {destinationContact?.BankAccountCount !== '0' &&
                     !accountValue && (
@@ -527,7 +528,7 @@ const TopUpModal = ({
               ) : (
                 <>
                   {!currentPhone &&
-                    currentProviderOption?.Category !== '7' && (
+                    currentProviderOption?.Category !== PAYPAL && (
                       <div className="add-new-phone">
                         <span>
                           {global.translate(
@@ -548,7 +549,7 @@ const TopUpModal = ({
                         />
                       </div>
                     )}
-                  {currentProviderOption?.Category === '7' && (
+                  {currentProviderOption?.Category === PAYPAL && (
                     <div className="add-new-phone">
                       <span>
                         {global.translate(
@@ -594,8 +595,8 @@ const TopUpModal = ({
                 </>
               )}
             </div>
-
-            {isSelfBuying && (
+            {currentProviderOption?.Category !== BANK_TRANSFER &&
+            isSelfBuying ? (
               <div className="dest-counties medium-padding-top">
                 <div className="small-padding-bottom">
                   {global.translate('Select a number')}
@@ -612,7 +613,7 @@ const TopUpModal = ({
                   setCurrentOption={setSelectedPhoneNumber}
                 />
               </div>
-            )}
+            ) : null}
           </Wrapper>
           <div className="money-sections">
             <div className="amount">
